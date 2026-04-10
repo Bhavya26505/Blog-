6e4b45b6c911fb15e09b82a0d94afd5ab415df6a
Your New Name <your.email@example.com>
2026-04-10 20:53:53 +0530

    new project database

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kkifwcoxrcuolyrgubxb.supabase.co";
+const supabaseUrl = "https://pdwbrfcrplntdpzqwzaf.supabase.co";
 const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtraWZ3Y294cmN1b2x5cmd1YnhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMDEzMzAsImV4cCI6MjA3MzY3NzMzMH0.vAsVWVRm3v2LRWM8T-Ej9vIsyecvJ6oyPmbvTSBBRWo";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkd2JyZmNycGxudGRwenF3emFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjAxODEsImV4cCI6MjA5MTM5NjE4MX0.4FoivU6PP3LOjCL77TXbbmOn5Qbj9edI7jOJ_8Mzkno";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
